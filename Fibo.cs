6898f34948ab741ce226fb6a4aa2ad6a4c1d7cae
lera <lera@rbi.ru>
2022-08-25 20:26:11 +0300

    Добавьте файлы проекта.

---
+++ Fibo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritms_FibonacciNumbers
+{
+    class Fibo
+    {
+        public long Fibo1(int N)
+        {
+            if (N == 1 || N == 2)
+                return 1;
+            return Fibo1(N - 1) + Fibo1(N - 2);
+        }
+
+        public long Fibo2(int N)
+        {
+            long f1 = 1;
+            long f2 = 1;
+            long f3=0;
+            for (int j=1;j<N;j++)
+            {
+                if (j < 3)
+                {
+                    f1 = 1;
+                    f2 = 1;
+                }
+
+                f3 = f1 + f2;
+                f1 = f2;
+                f2 = f3;
+            }
+
+            if (N < 3)
+                return 1;
+           return f3;
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Algoritms_FibonacciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Fibo f = new Fibo();
+
+            for (int i =1;i<10;i++)
+            {
+                Console.WriteLine($" число Фибоначи для {i} : {f.Fibo1(i)}");
+            }
+
+            Console.WriteLine("");
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine($" число Фибоначи2 для {i} : {f.Fibo2(i)}");
+            }
+
+        }
+    }
+}
